bde6cf87daece4436649ae486dd5bbbb30f68e39
kelly <kelly.tate.dev@gmail.com>
2022-11-12 08:16:30 -0500

    Passes Wave 3

--- adagrams/game.py
+++ adagrams/game.py
@@ -135,7 +135,47 @@ def uses_available_letters(word, letter_bank):
     return valid
 
 def score_word(word):
-    pass
+    
+    word = word.upper()
+
+    points = 0
+
+    score_chart = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+    }
+
+    for letter in word:
+        points += score_chart[letter]
+
+    if len(word) >= 7 and len(word) <= 10:
+        points += 8
+
+    return points
 
 def get_highest_word_score(word_list):
     pass
